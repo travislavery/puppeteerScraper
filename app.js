414bb93d57a454204822157ebe6d0e411cd78f83
TravisLavery <travisalavery@gmail.com>
2020-07-18 23:14:14 -0600

    add json step files

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const puppeteer = require('puppeteer');
+const steps = require('./steps.json');
+const stepDetails = require('./amazonScrape.json');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
-
+  //await page.goto('https://amazon.com');
+  //await page.screenshot({path: 'example.png'});
+  console.log(steps)
   await browser.close();
 })();
