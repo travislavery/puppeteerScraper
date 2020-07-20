73329cda42b92167385dceff827c75f2f722ec44
TravisLavery <travisalavery@gmail.com>
2020-07-20 17:35:03 -0600

    stop console logging each step

--- app.js
+++ app.js
@@ -14,18 +14,7 @@ const main = (async () => {
             const someSteps = linkedSteps.map(step => {
                 step.puppetPage = page;
                 step.puppetBrowser = browser;
-                console.log(step)
                 return step
-                // if (step.stepDetail.type == 'Connect') {
-                    
-                //     await Promise.all([
-                //         await connectTo(step.stepDetail.details.url),
-                //         ,
-                //     ])
-                // } else if (step.stepDetail.type == 'Disconnect') {
-                    
-                //     await browser.close()
-                // }
             })
             return someSteps
         }
@@ -33,18 +22,9 @@ const main = (async () => {
         async function runSteps(stepArray){
             for (const i in stepArray) {
                 const element = await stepArray[i].run();
-                console.log(element)
+                //console.log(element)
             }
-            // stepArray.forEach(step => {
-            //     runStep(step)
-            // })
         }
-        
-        
-      
-        //await page.goto('https://amazon.com');
-        //await page.screenshot({path: 'example.png'});
-        
     }
     catch(err){
         console.log(err)
